68d6509ff73f25a8b858376aabf9200912ce26bf
vdblm <balazadehvahid@gmail.com>
2023-11-08 11:48:06 -0500

    reduce memory usage

--- src/rollout.py
+++ src/rollout.py
@@ -23,36 +23,34 @@ def policy_rollout(policy_fn, rng_key, mu_vectors, horizon):
     """
     n_envs = mu_vectors.shape[0]
 
+    @jax.remat
     def policy_step(state_input, _):
-        i, time_steps, actions, rewards, rng = state_input
+        i, time_steps, actions, rewards, rngs = state_input
         # TODO make the transformer input dynamic
-        # shape: (n_envs, num_actions)
-        rng, key = jax.random.split(rng)
 
         # shape: (n_envs, num_actions)
-        log_prob = policy_fn(key, time_steps, actions, rewards)
-        rng, key = jax.random.split(rng)
-        new_action = jax.random.categorical(key, log_prob)
+        log_prob = policy_fn(rngs[i, 0], time_steps, actions, rewards)
+        new_action = jax.random.categorical(rngs[i, 1], log_prob)
 
-        rng, key = jax.random.split(rng)
         new_reward = jax.random.bernoulli(
-            key, mu_vectors[jnp.arange(n_envs), new_action]
+            rngs[i, 2], mu_vectors[jnp.arange(n_envs), new_action]
         )
 
         actions = actions.at[:, i + 1].set(new_action)
         time_steps = time_steps.at[:, i + 1].set(i + 1)
         rewards = rewards.at[:, i + 1].set(new_reward)
-        carry = (i + 1, time_steps, actions, rewards, rng)
+        carry = (i + 1, time_steps, actions, rewards, rngs)
         return carry, log_prob
 
     # The first step is considered 0 with zero action and reward
     # However, history starts from 1
+    rngs = jax.random.split(rng_key, 3 * (horizon + 1)).reshape(horizon + 1, 3, -1)
     init_val = (
         0,
         jnp.zeros((n_envs, horizon + 1), dtype=jnp.int32),
         jnp.zeros((n_envs, horizon + 1), dtype=jnp.int32),
         jnp.zeros((n_envs, horizon + 1), dtype=jnp.float32),
-        rng_key,
+        rngs,
     )
 
     (_, _, actions, rewards, _), log_probs = jax.lax.scan(
