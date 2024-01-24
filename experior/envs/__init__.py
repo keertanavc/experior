f845ce2841f435b33667f15f621c595746f00f8c
viet <viet@cs.toronto.edu>
2024-01-24 13:52:28 -0500

    changed env inits

--- experior/envs/__init__.py
+++ experior/envs/__init__.py
@@ -4,3 +4,4 @@ from .bandit import BayesStochasticBandit, MetaParam, Param
 from .cartpole_env import CartPole
 from .ds import DeepSea
 from .breakout import Breakout
+from .brax_wrappers import BraxGymnaxWrapper, ClipAction, VecEnv, NormalizeVecObservation, NormalizeVecObsEnvState, NormalizeVecReward, NormalizeVecRewEnvState
